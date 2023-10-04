fc9e9dac6528653c2d6330f824b7cc532c9ab46c
dr-vpantel <vpantel@mail.ru>
2023-10-04 14:59:59 +0300

    Task completed

---
+++ Task/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] LessThanFourSymbolsArray (string[] array)
+{
+    int n = 3;
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            size++;
+        }
+    }
+    string[] arrayTwo = new string[size];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            arrayTwo[j] = array[i];
+            j++;
+        }
+    }
+    return arrayTwo;
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] firstArray = new string[4] {"Hello", "2", "world", ":-)"};
+Console.WriteLine("Первоначальный массив:");
+PrintArray(firstArray);
+Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
+PrintArray(LessThanFourSymbolsArray(firstArray));
